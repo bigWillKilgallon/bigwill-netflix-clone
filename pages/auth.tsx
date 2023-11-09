81a89e580a8aee3129635ac55b5f201116f11f5c
BigWill Kilgallon <_@bigwill.dev>
2023-11-09 06:58:06 +0000

    login method now called from auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Auth = () => {
+    const router = useRouter();
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
@@ -15,6 +18,21 @@ const Auth = () => {
         );
     }, []);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: "/",
+            });
+
+            router.push("/");
+        } catch (error) {
+            console.error(error);
+        }
+    }, [email, password, router]);
+
     const register = useCallback(async () => {
         try {
             await axios.post("/api/register", {
@@ -22,10 +40,11 @@ const Auth = () => {
                 name,
                 password,
             });
+            login();
         } catch (error) {
             console.error(error);
         }
-    }, [email, name, password]);
+    }, [email, name, password, login]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -72,7 +91,7 @@ const Auth = () => {
                         </div>
 
                         <button
-                            onClick={register}
+                            onClick={variant === "login" ? login : register}
                             className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
                         >
                             {variant === "login" ? "Login" : "Sign up"}
